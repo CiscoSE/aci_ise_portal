3504531e01aaae2737a3f4372388146cfd6ae0ab
jasmah <jasmah@gmail.com>
2019-05-16 11:28:53 -0600

    add updated Licensing

--- web_ui/controllers/db.py
+++ web_ui/controllers/db.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2018 Cisco and/or its affiliates.
+Copyright (c) 2019 Cisco and/or its affiliates.
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.0 (the "License"). You may obtain a copy of the
 License at
